e4d953463a0608ffdc86170152a40b804eb4153c
Александр Истомин <67789335+IstominAlexander@users.noreply.github.com>
2021-10-30 16:44:12 +0500

    Add files via upload

---
+++ N1.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+using namespace std;
+int number;
+int a, b, c;
+bool F = true;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	while (F)
+	{
+		cout << "Введите число от 100 до 999: ";
+		cin >> number;
+		a = number / 100;
+		number = number % 100;
+		b = number / 10;
+		c = number % 10;
+		number = a * 100 + b * 10 + c;
+		if (number >= 100)
+		{
+			if (number <= 999) 
+			{
+				if (a != b)
+				{
+					if (a != c)
+					{
+						if (b != c)
+						{
+							F = false;
+							cout << a << b << c << endl;
+							cout << a << c << b << endl;
+							cout << b << a << c << endl;
+							cout << b << c << a << endl;
+							cout << c << a << b << endl;
+							cout << c << b << a << endl;
+							cout << "Самое большое число: ";
+							if (a > b)
+							{
+								if (a > c)
+								{
+									if (b > c)
+									{
+										cout << a << b << c;
+									}
+									else
+									{
+										cout << a << c << b;
+									}
+								}
+							}
+							else if (c > a)
+							{
+								if (c > b)
+								{
+									if (b > a)
+									{
+										cout << c << b << a;
+									}
+									else
+									{
+										cout << c << a << b;
+									}
+								}
+
+							}
+							else if (b > a)
+							{
+								if (b > c)
+								{
+									if (c > a)
+									{
+										cout << b << c << a;
+									}
+									else
+									{
+										cout << b << a << c;
+									}
+								}
+
+							}
+						}
+						else
+						{
+							cout << "Цирфы в числе не должны повторяться!" << endl;
+						}
+
+					}
+					else
+					{
+						cout << "Цирфы в числе не должны повторяться!" << endl;
+					}
+				}
+				else
+				{
+					cout << "Цирфы в числе не должны повторяться!" << endl;
+				}
+			}
+		}
+	}
+	
+}
---
+++ N12.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int number, i, a;
+bool F = true;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	number = rand() % 100;
+	number = number + 1;
+	number = 10;
+	for (i = 6; i > 0; i--)
+	{
+		if (F)
+		{
+			cout << "У вас осталось " << i << " попыток" << endl << "Угадайте число от 1 до 100: ";
+			cin >> a;
+			cout << endl;
+			if (a == number)
+			{
+				cout << "Вы угадали число!";
+				F = false;
+			}else if(number > a)
+			{
+				cout << "Загаданное число больше введенного вами" << endl;
+			}
+			else if (number < a)
+			{
+				cout << "Загаданное число меньше введенного вами" << endl;
+			}
+		}
+	}
+	if (F)
+	{
+		cout << "К сожалению, вы не угадали число";
+	}
+}
